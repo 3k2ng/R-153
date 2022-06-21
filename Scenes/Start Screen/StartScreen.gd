25bbfc3468a135e34acfb6b23293d24ab38090da
nikolai-nesterov <niknester2002@gmail.com>
2022-06-21 17:03:00 -0600

    added better start screen background

--- Scenes/Start Screen/StartScreen.gd
+++ Scenes/Start Screen/StartScreen.gd
@@ -1,12 +1,23 @@
 extends Control
 
 onready var _start_game = $"Menu/Start Game"
+onready var _quit = $"Menu/Quit"
 onready var _transition_rect = $SceneTransitionRect
 
-func _ready() -> void:
-	_start_game.grab_focus()
+var _focused := false
 
-
+func _input(event: InputEvent) -> void:
+	if not _focused:
+		if event.is_action_pressed("Down"):
+			_quit.grab_focus()
+			_focused = true
+		elif event.is_action_pressed("Up") or event.is_action_pressed("Left") or event.is_action_pressed("Right"):
+			_start_game.grab_focus()
+			_focused = true
+			
 
 func _on_Start_Game_pressed() -> void:
 	_transition_rect.transition_to()
+
+func _on_Quit_pressed() -> void:
+	get_tree().quit()
