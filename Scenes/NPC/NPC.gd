ce84207b6ad59fa5c235038273ced6c2c9fc7697
Crackypenguins <jacky.tran1@ucalgary.ca>
2022-06-26 12:35:00 -0600

    NPC just needs killzone interaction, will get done next hour after lunch

--- Scenes/NPC/NPC.gd
+++ Scenes/NPC/NPC.gd
@@ -5,10 +5,13 @@ var velocity = Vector2()
 # Determines the npc's starting direction
 export (int) var  direction = 1
 var nearby_bodies = null
+var computers = null
 var new_direction = null
-var state = State.Computing
+var detected = null
+var hit_pos = null
+var detection = null
+var state = State.Patrolling
 var alert = Alert.Normal
-var de_agroTimer_running = false
 
 enum Alert {
 	Normal
@@ -22,17 +25,16 @@ enum State {
 	Computing
 	Patrolling
 	Chasing
+	Dead
 	}
 
 # Runs before the program starts
 func _ready():
+	# Positions the edge detection according to the starting direction
+	$FloorLine.position.x = ($NPCcollisionBox.shape.get_extents().x / 5) * direction
 	# Checks if the sprite needs to be flipped
 	if direction == 1:
 		$NPCsprite.flip_h = true
-	# Positions the edge detection according to the starting direction
-	$DetectionLines/FloorLine.position.x = ($NPCcollisionBox.shape.get_extents().x / 5)
-	# Shifts the detection nodes according to the starting direction
-	$DetectionLines.scale.x = $DetectionLines.scale.x * direction
 
 
 # Function that's called every frame. Basically modifies npc behavior in real time
@@ -44,21 +46,25 @@ func _physics_process(_delta):
 	_animation()
 	# Applies the horizontal movement speed in the intended direction
 	_move_velocity()
-	if state == State.Patrolling:
-		print($De_agroTimer.time_left)
+	
 	if $De_agroTimer.time_left < 0.2:
 		$De_agroTimer.stop()
+	if nearby_bodies:
+		detect()
 
 
 func _change_state():
 	if state != State.Chasing:
-		if $DetectionLines/DetectionLine.is_colliding() && $DetectionLines/DetectionLine.get_collider().get_name() == "Player":
+		if detected:
 			alert = Alert.Question
 			yield(get_tree().create_timer(1.0), "timeout")
 			alert = Alert.Alert
 			state = State.Chasing
 	if state == State.Patrolling && $De_agroTimer.is_stopped():
 		alert = Alert.Normal
+		if computers != null:
+			state = State.Computing
+		
 		############ HOW TO INTERACT WITH COMPUTER NOW THAT IT IS AN AREA2D?#########
 
 
@@ -67,17 +73,23 @@ func chooseDirection():
 	# Determines direction when the NPC is actively patrolling
 	if state == State.Patrolling:
 		# Changes the direction when the NPC either hits a wall or approaches an edge
-		if is_on_wall() || not $DetectionLines/FloorLine.is_colliding():
+		if is_on_wall() || not $FloorLine.is_colliding():
 			_switch_directions()
-
+		if $De_agroTimer.is_stopped():
+			find_computer()
 
 	# Determines direction when the NPC is chasing the player
 	if state == State.Chasing:
 		# If the player is within detection range, sets direction towards the player
-		if $DetectionLines/DetectionLine.is_colliding() && $DetectionLines/DetectionLine.get_collider().get_name() == "Player":
-			direction = stepify(position.direction_to($DetectionLines/DetectionLine.get_collider().position).x, 1)
+		if detected:
+			new_direction = stepify(position.direction_to(nearby_bodies.position).x, 1)
+			if new_direction == 0:
+				print("above!")
+				$NPCsprite.play("Idle")
+			elif new_direction != direction:
+				_switch_directions()
 		# If the NPC hits wall or ledge when chasing the player, just stops
-		elif is_on_wall() || not $DetectionLines/FloorLine.is_colliding():
+		elif is_on_wall() || not $FloorLine.is_colliding():
 			$NPCsprite.play("Idle")
 		# If the NPC is still on alert but player leaves detection
 		elif alert != Alert.Caution:
@@ -101,9 +113,9 @@ func _move_velocity():
 	velocity.y += 30
 	# If the NPC is in a moving state
 	if state == State.Patrolling || state == State.Chasing:
-		velocity.x = direction * 400
+		velocity.x = direction * 50
 	# If the NPC is in a stationary state
-	if state == State.Idle || state == State.Computing:
+	if state == State.Idle || state == State.Computing || state == State.Dead:
 		velocity.x = 0
 	# Apply the new velocity
 	velocity = move_and_slide(velocity, Vector2.UP)
@@ -120,9 +132,14 @@ func _animation():
 	# Plays when the NPC is moving
 	elif state == State.Patrolling || state == State.Chasing && !is_on_wall():
 		$NPCsprite.play("Move")
+	# Plays when the NPC dies
+	elif state == State.Dead:
+		$NPCsprite.play("Death")
+		yield(get_tree().create_timer(1.5), "timeout")
+		queue_free()
 	
 	# Normal phase
-	if alert == Alert.Normal:
+	if alert == Alert.Normal || state == State.Dead:
 		$AlertState.play("NotAware")
 	# Sees something or is confused
 	elif alert == Alert.Question:
@@ -141,20 +158,42 @@ func _switch_directions():
 	direction = direction * -1
 	# Flips the sprite model
 	$NPCsprite.flip_h = not $NPCsprite.flip_h
-	# Flips the detection boxes
-	$DetectionLines.scale.x = $DetectionLines.scale.x * -1
+	# Flips the edge detection
+	$FloorLine.position.x = $FloorLine.position.x * -1
 
 
-# Detects if the player enters the NPCs field of sight
-#func _on_DetectionBoxes_body_entered(body):
-#	nearby_bodies = body
-#	state = Alert.Question
-#	print("Found: ", nearby_bodies)
+func detect():
+	var space_state = get_world_2d().direct_space_state
+	var result = space_state.intersect_ray(position, nearby_bodies.position, [self], collision_mask)
+	if result:
+		hit_pos = result.position
+		if result.collider.name == "Player":
+			detected = true
+		else:
+			detected = false
+
+func find_computer():
+	if $SearchComputerLeft.is_colliding() && $SearchComputerLeft.get_collider() == "ComputerBody":
+		if direction != -1:
+			_switch_directions()
+		elif direction != 1:
+			_switch_directions()
+
+func die():
+	state = State.Dead
+	
+func _on_Detection_Radius_body_entered(body):
+	nearby_bodies = body
+
+func _on_Detection_Radius_body_exited(body):
+	nearby_bodies = null
+	detected = false
 
 
-# Detects if the player leaves the NPCs field of sight
-#func _on_DetectionBoxes_body_exited(body):
-#	nearby_bodies = null
-#	$De_agroTimer.start()
-#	$De_agroTimer.stop()
-#	print("Exited: ", nearby_bodies)
+func _on_Compute_body_entered(body):
+	if body.get_name() == "ComputerBody":
+		computers = body
+
+
+func _on_Compute_body_exited(body):
+	computers = null
