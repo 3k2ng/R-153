c100ff38adb32c0839daf7d20d7001ca8a4cd829
nikolai-nesterov <niknester2002@gmail.com>
2022-06-29 00:16:03 -0600

    quick settings fix

--- Scenes/Start Screen/Settings/Settings.gd
+++ Scenes/Start Screen/Settings/Settings.gd
@@ -9,10 +9,11 @@ func _ready() -> void:
 
 
 func _on_MusicSlider_changed(value : float) -> void:
-	_music_label.text = str(value) + "%"
 	if value == 0:
 		_soundtrack.playing = false
+		_music_label.text = "OFF"
 	else:
 		if not _soundtrack.playing:
 			_soundtrack.playing = true
+		_music_label.text = str(value) + "%"			
 		_soundtrack.volume_db = (0 - (100 - value)) * 0.2
